07db509fd64d233560fbb890c97888a42750b856
kelvin <164617970@qq.com>
2020-04-09 08:02:27 -0700

    fist test

--- main.c
+++ main.c
@@ -14,27 +14,31 @@ int main()
 
     while(1)
     {
-        if(TIM1_1MS(TIM1_WORK,100))
+        if(TIM1_1MS(TIM1_WORK,2))
         {
-            printf("%d",MULL_WORK.MULL_LED.MULL_LED_HSV.H);
-            if(Test_flag)
-                MULL_WORK.MULL_LED.MULL_LED_HSV.H --;
-            else
-            {
-                 MULL_WORK.MULL_LED.MULL_LED_HSV.H++;
-            }
-            if(MULL_WORK.MULL_LED.MULL_LED_HSV.H <= 0)
-                Test_flag = false;
-            else if (MULL_WORK.MULL_LED.MULL_LED_HSV.H >= 360)
-            {
-                Test_flag = true;
-            }
+            printf("%d\t",MULL_WORK.MULL_LED.MULL_LED_HSV.H);
 
-             HSVtoRGB(&MULL_WORK.MULL_LED.MULL_LED_HSV,&MULL_WORK.MULL_LED.MULL_LED_RGB) ;
+
+             HSVtoRGB(&MULL_WORK.MULL_LED.MULL_LED_RGB,&MULL_WORK.MULL_LED.MULL_LED_HSV) ;
+
+             if(Test_flag)
+                 MULL_WORK.MULL_LED.MULL_LED_HSV.H --;
+             else
+             {
+                  MULL_WORK.MULL_LED.MULL_LED_HSV.H++;
+             }
+             if(MULL_WORK.MULL_LED.MULL_LED_HSV.H <= 0)
+                 Test_flag = false;
+             else if (MULL_WORK.MULL_LED.MULL_LED_HSV.H >= 360)
+             {
+                 Test_flag = true;
+             }
+             printf("\tIN!!!\n");
 
              TIM1_StatusClear(TIM1_WORK);
 
         }
+//        printf("TIM1_WOKING[TIM1_WORK] = %d \n",TIM1_WOKING[TIM1_WORK]);
 
     }
 
